a2f121bea29c42e8dd39869eb3a6907e44315656
Maria <mia.gotcha@proton.me>
2026-05-08 09:58:58 +0200

    delete fetcher and swr in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,10 @@
 import GlobalStyle from "../styles";
-import { SWRConfig, fetcher } from "swr";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <SWRConfig
-        value={{
-          fetcher,
-          refreshInterval: 1000,
-        }}
-      >
-        <GlobalStyle />
-        <Component {...pageProps} />
-      </SWRConfig>
+      <GlobalStyle />
+      <Component {...pageProps} />
     </>
   );
 }
